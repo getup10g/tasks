8a3df30847f5e02d24793105ee3d35fd963c46a4
getup10g <getup@10g.pl>
2018-04-22 11:00:38 +0100

    crossPolicy

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/v1/trello")
