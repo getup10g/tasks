3a9925f50529b631c51f9730b65defabac55a72d
getup10g <getup@10g.pl>
2018-04-28 13:06:25 +0100

    scheduler

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -14,7 +14,14 @@ public interface TaskRepository extends CrudRepository<Task, Long> {
     @Override
     Task save(Task task);
 
+
     Optional<Task> findById(Long id);
+
+
+    void deleteById(Long id);
+
+    @Override
+    long count();
 
 }
 
